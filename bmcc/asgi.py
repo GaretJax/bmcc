4525d5f055f3c2b84c30de7c5232b9e8485fead4
Jonathan Stoppani <jonathan@stoppani.name>
2025-11-21 23:04:26 +0000

    chore: add github deployment workflow

--- bmcc/asgi.py
+++ bmcc/asgi.py
@@ -7,14 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 
-# from django.core.asgi import get_asgi_application
-# application = get_asgi_application()
+from django.core.asgi import get_asgi_application
 
 
-# Workaround for this: https://github.com/encode/starlette/issues/664#issuecomment-698771946
-from django.core.wsgi import get_wsgi_application
-
-from a2wsgi import WSGIMiddleware
-
-
-application = WSGIMiddleware(get_wsgi_application())
+application = get_asgi_application()
