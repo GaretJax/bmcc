c709c3baa1c602683ac876dc326012df0d5be1dd
Jonathan Stoppani <jonathan@stoppani.name>
2025-11-23 04:53:39 +0000

    feat: autoreload kml

--- bmcc/missions/views.py
+++ bmcc/missions/views.py
@@ -1,5 +1,8 @@
+from datetime import timedelta
+
 from django.http import HttpResponse
 from django.urls import reverse
+from django.utils import timezone
 
 import simplekml
 
@@ -7,29 +10,19 @@ from ..tracking.models import Beacon
 from . import models
 
 
-KML = """
-<?xml version="1.0" encoding="UTF-8"?>
-<kml xmlns="http://earth.google.com/kml/2.1">
-  <NetworkLink>
-    <name>Loads Point.kml</name>
-    <Link>
-      <href>{url}</href>
-    </Link>
-  </NetworkLink>
-</kml>
-"""
-
-
 def kml_entrypoint(request, mission_id):
     mission = models.Mission.objects.get(pk=mission_id)
 
-    kml = KML.strip().format(
-        url=request.build_absolute_uri(
-            reverse("missions:updating_kml", kwargs={"mission_id": mission.pk})
-        )
+    kml = simplekml.Kml()
+    netlink = kml.newnetworklink(
+        name=mission.name,
     )
+    netlink.link.href = request.build_absolute_uri(
+        reverse("missions:updating_kml", kwargs={"mission_id": mission.pk})
+    )
+    netlink.link.refreshmode = "onExpire"
 
-    response = HttpResponse(kml)
+    response = HttpResponse(kml.kml())
     filename = f"{mission.pk}.kml"
     response["Content-Disposition"] = f'attachment; filename="{filename}"'
     return response
@@ -39,6 +32,7 @@ def kml_update(request, mission_id):
     mission = models.Mission.objects.get(pk=mission_id)
 
     kml = simplekml.Kml()
+    kml.networklinkcontrol.expires = timezone.now() + timedelta(seconds=15)
 
     for beacon in Beacon.objects.active().filter(asset__mission=mission):
         ping = beacon.pings.order_by("-created_at").first()
